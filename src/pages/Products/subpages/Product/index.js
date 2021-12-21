3f70fda0a1010866394fa703e55768da509cf8cb
Jonas Cavalcante <jonas.cavalcante@workdb.com.br>
2021-12-21 07:50:54 -0300

    feat: add useNavigate

--- src/pages/Products/subpages/Product/index.js
+++ src/pages/Products/subpages/Product/index.js
@@ -1,14 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
 import { Menu } from "../../../../components";
 
 const Product = () => {
 
   const paramns = useParams();
+  const navigate = useNavigate();
+
+  const handleNavigate = (page) => {
+    switch (page) {
+      case 'back':
+        navigate(-1)
+        break;
+
+      case 'home':
+        navigate('/')
+        break;
+
+      default:
+        break;
+    }
+  }
 
   return (
     <section>
       <Menu />
       <h2>Product{paramns.id}</h2>
+      <button onClick={() => handleNavigate('back')}>Back</button>
+      <button onClick={() => handleNavigate('home')}>Home</button>
     </section>
   )
 }
